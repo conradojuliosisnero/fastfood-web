be533624a7cde293cd16c07e2f4c8e27f65c86a5
conrado julio <conradojuliosisinero@gmail.com>
2023-06-05 16:17:11 -0500

    agregando mas estilos al sitio

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// const menDisplay = document.querySelector('.nav');
+// const tittleNav = document.querySelector('.nav');
+
+// window.onscroll = ()=>{
+//         if (window.pageYOffset>40) {
+//             menDisplay.style.background='#FFCC00';
+//             menDisplay.style.borderBottom='solid 2px #FFFF';
+//             // tittleNav.style.display='none';
+//         }else{
+//             menDisplay.style.background='transparent';
+//         }
+// }
